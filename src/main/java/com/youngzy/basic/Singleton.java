e7b11a6a3f1890beacbb6a0f878a5ab1fb325000
young-win <young.zhu.2012@gmail.com>
2023-04-28 14:40:19 +0800

    Singleton

---
+++ src/main/java/com/youngzy/basic/Singleton.java
@@ -0,0 +1,22 @@
+package com.youngzy.basic;
+
+/**
+ * Lazy Initialization Holder Class 惯用法
+ *
+ * 由 Java 规范保证：
+ * - Class Loading 懒加载
+ * - 初始化过程线程安全
+ *
+ * @author youngzy
+ * @since 2023-04-28
+ */
+public class Singleton {
+    private final static class SingletonHolder {
+        // 在 SingletonHolder.<clinit> 中初始化
+        private static Singleton instance = new Singleton();
+    }
+
+    public static Singleton instance() {
+        return SingletonHolder.instance;
+    }
+}
---
+++ src/main/java/com/youngzy/basic/SingletonDCL.java
@@ -0,0 +1,26 @@
+package com.youngzy.basic;
+
+/**
+ * DCL: Double-Checked Locking
+ *
+ * Java 1.5 之后的内存模型才能保证正确工作
+ *
+ * @author youngzy
+ * @since 2023-04-28
+ */
+public class SingletonDCL {
+    private static volatile SingletonDCL instance = null;
+
+    private SingletonDCL() {}
+
+    public static SingletonDCL getInstance() {
+        if (instance == null) {
+            synchronized (SingletonDCL.class) {
+                if (instance == null) {
+                    instance = new SingletonDCL();
+                }
+            }
+        }
+        return instance;
+    }
+}
