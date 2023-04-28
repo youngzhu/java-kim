5772a718be353576ad0d58b708f1a1519445767f
young-win <young.zhu.2012@gmail.com>
2023-04-28 15:04:59 +0800

    volatile 不能保证原子性

---
+++ src/main/java/com/youngzy/basic/VolatileAtomicity.java
@@ -0,0 +1,33 @@
+package com.youngzy.basic;
+
+/**
+ * volatile 不能保证原子性
+ * 最终 counter < 20000
+ *
+ * @author youngzy
+ * @since 2023-04-28
+ */
+public class VolatileAtomicity {
+    private static volatile int counter = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                counter++;
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                counter++;
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("counter: " + counter);
+    }
+}
